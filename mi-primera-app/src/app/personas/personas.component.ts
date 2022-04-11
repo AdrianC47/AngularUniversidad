94dca811a2b201ef0aa7596616c7a96a7e244301
Adrian <luiscabreraucme@hotmail.com>
2022-04-11 11:15:30 -0500

    Aplicando Estilos CSS en Angular

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -3,12 +3,18 @@ import { Component } from "@angular/core";
 @Component ({//Decorador de la Clase PersonasComponent
     selector: 'app-personas' , //con esta propiedad/atributo  vamos a indicar el nombre del componente y el nombre se lo va a
     //usar en las paginas html para incluir al componente 
-    //templateUrl: './personas.component.html' //aqui se define un archivo para incluir el texto html que queremos agregar en el componente
-    template: `<h1>Listado de Personas</h1>
-    <app-persona></app-persona>
-    <app-persona></app-persona>`// Esto se lo conoce como componente en linea , lo cual se recomienda usar solo cuando se tengan pocas lineas html
+    templateUrl: './personas.component.html',//aqui se define un archivo para incluir el texto html que queremos agregar en el componente
+     // template: `<h1>Listado de Personas</h1>
+    // <app-persona></app-persona>
+    // <app-persona></app-persona>`// Esto se lo conoce como componente en linea , lo cual se recomienda usar solo cuando se tengan pocas lineas html
+    //Las hojas de estilo tambien se pueden definir en linea
+    // styleUrls: ['./personas.component.css'] 
+    styles: [`
+        h1{
+            color:blue;
+        }
+    `]
 })
-
 export class PersonasComponent{ //clase 
 
 }
