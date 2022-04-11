e8f9e5c5b268d98c99857833ca6c029c49b7d95f
Adrian <luiscabreraucme@hotmail.com>
2022-04-11 01:17:09 -0500

    Creacion de Componentes en Angular con CLI

--- mi-primera-app/src/app/app.module.ts
+++ mi-primera-app/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
 
 //En los decoradores se indican  cuales son los componentes que se usaran en nuestra aplicación
 @NgModule({ // esto es un decorador, los decoradores se usan para agregar características extra a la definicion de la clase AppModule
   declarations: [ //atributo
-    AppComponent, PersonasComponent    //AppComponent es un componente por default
+    AppComponent, PersonasComponent, PersonaComponent    //AppComponent es un componente por default
   ],
   imports: [ //atributo
     BrowserModule
---
+++ mi-primera-app/src/app/persona/persona.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ //decorador
+  selector: 'app-persona', //selector
+  templateUrl: './persona.component.html', //plantilla
+  styleUrls: ['./persona.component.css'] //hoja de estilo
+})
+export class PersonaComponent   {
+
+  constructor() { }
+
+ 
+
+}
