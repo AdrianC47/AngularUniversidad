3d7448d3c0d23f2991bed62427ea9e409fdc83bb
Adrian <luiscabreraucme@hotmail.com>
2022-05-22 17:23:09 -0500

    Referencia Local en Angular

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -9,11 +9,11 @@ import { Persona } from '../persona.model';
 export class FormularioComponent  {
 
   @Output() personaCreada = new EventEmitter<Persona>(); //con esto  crearemos un evento que propagará el objeto Persona al componente padre app
-  nombreInput:string = '';
-  apellidoInput:string = '';
+  //nombreInput:string = '';
+  //apellidoInput:string = '';
 
-  agregarPersona(){
-    let persona1=new Persona(this.nombreInput,this.apellidoInput);
+  agregarPersona(nombreInput: HTMLInputElement,apellidoInput:HTMLInputElement){
+    let persona1=new Persona(nombreInput.value,apellidoInput.value); //ya no se esta usando el two way binding
   //  this.personas.push(persona1);
     this.personaCreada.emit(persona1)//con el emit lo propagamos al padre
   }
