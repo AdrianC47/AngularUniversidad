1eed54769052b16f34b7cedc8d0564673eec1bda
Adrian <luiscabreraucme@hotmail.com>
2022-06-14 16:47:54 -0500

    Servicios en Angular

--- listado-personas/src/app/app.component.ts
+++ listado-personas/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Persona } from './persona.model';
+import { LoggingService } from './LoggingService.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,13 @@ export class AppComponent {
   titulo = 'Listado de Personas';
   personas:Persona[]=[new Persona("Adrian","Cabrera"), new Persona("Fernando", "Herrera"), new Persona("Vladimir", "Robles")];
 
+
+  constructor(private loggingService:LoggingService){
+    
+  }
+
   personaAgregada(persona:Persona){
+    this.loggingService.enviarMensajeAConsola("agregamos al arreglo la nueva persona:" + persona.nombre + " apellido: " +persona.apellido);
     this.personas.push(persona);
   }
 
--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -1,11 +1,13 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model';
+import { LoggingService } from '../LoggingService.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
-})
+  styleUrls: ['./formulario.component.css'],
+  //providers: [LoggingService] //se añade un proveedor para poder usar un servicio
+})  
 export class FormularioComponent  {
 
   @Output() personaCreada = new EventEmitter<Persona>(); //con esto  crearemos un evento que propagará el objeto Persona al componente padre app
@@ -15,11 +17,16 @@ export class FormularioComponent  {
   @ViewChild('nombreInput')nombreInputa: ElementRef; //el primero es nombre de la referencia local(en el html) y el segundo es el nombre del atributo
   @ViewChild('apellidoInput')apellidoInputa: ElementRef;
 
+  constructor(private loggingService:LoggingService){}//Concepto de inyeccion de dependencias a través del constructor
+
+
+  ngOnInit() {    
+  }
+   
   // agregarPersona(nombreInput: HTMLInputElement,apellidoInput:HTMLInputElement){ esto es en el locale reference
     agregarPersona( ){
-    let persona1=new Persona
-    (this.nombreInputa.nativeElement.value,
-    this.apellidoInputa.nativeElement.value); // Esto es propio del view child (el native element)
+    let persona1=new Persona(this.nombreInputa.nativeElement.value,this.apellidoInputa.nativeElement.value); // Esto es propio del view child (el native element)
+    this.loggingService.enviarMensajeAConsola('Enviamos persona con nombre: ' +persona1.nombre+" apellido:" +persona1.apellido);//llamo al método de mi servicio
     this.personaCreada.emit(persona1)//con el emit lo propagamos al padre
   }
  
