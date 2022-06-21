d388a65be78c6642452548de82aec2cd0a70e677
Adrian <luiscabreraucme@hotmail.com>
2022-06-21 17:18:26 -0500

    Creacion de Clases de Modelo y Servicios

---
+++ presupuesto-app/src/app/egreso/egreso.servicio.ts
@@ -0,0 +1,8 @@
+import { Egreso } from "./egreso.model";
+
+export  class EgresoServicio{
+    egresos:Egreso[]=[
+      new Egreso ("Celular Nuevo",900),
+      new Egreso ("Ropa",435.28)  
+    ];
+}
