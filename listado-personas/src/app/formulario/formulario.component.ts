a0d9b075e24af75e5f1d927522de95e269638a1f
Adrian <luiscabreraucme@hotmail.com>
2022-06-21 12:43:20 -0500

    Mejorando visualmente la app personas

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -12,11 +12,11 @@ import { PersonasService } from '../personas.service';
 export class FormularioComponent  {
 
   // @Output() personaCreada = new EventEmitter<Persona>(); //con esto  crearemos un evento que propagará el objeto Persona al componente padre app
-  //nombreInput:string = '';
-  //apellidoInput:string = '';
+  nombreInput:string = '';
+  apellidoInput:string = '';
 
-  @ViewChild('nombreInput')nombreInputa: ElementRef; //el primero es nombre de la referencia local(en el html) y el segundo es el nombre del atributo
-  @ViewChild('apellidoInput')apellidoInputa: ElementRef;
+  //@ViewChild('nombreInput')nombreInputa: ElementRef; //el primero es nombre de la referencia local(en el html) y el segundo es el nombre del atributo
+  //@ViewChild('apellidoInput')apellidoInputa: ElementRef;
 
   constructor(private loggingService:LoggingService, private personasService:PersonasService){//Concepto de inyeccion de dependencias a través del constructor
       //Lo siguiente se lo hace con el fin de demostrar como me comunico por diferentes componentes por medio de un servicio
@@ -33,7 +33,7 @@ export class FormularioComponent  {
    
   // agregarPersona(nombreInput: HTMLInputElement,apellidoInput:HTMLInputElement){ esto es en el locale reference
     agregarPersona( ){
-    let persona1=new Persona(this.nombreInputa.nativeElement.value,this.apellidoInputa.nativeElement.value); // Esto es propio del view child (el native element)
+    let persona1=new Persona(this.nombreInput,this.apellidoInput); // Esto es propio del view child (el native element)
     //this.loggingService.enviarMensajeAConsola('Enviamos persona con nombre: ' +persona1.nombre+" apellido:" +persona1.apellido);//llamo al método de mi servicio
     //this.personaCreada.emit(persona1)//con el emit lo propagamos al padre
     this.personasService.agregarPersona(persona1);
