2bb25be4f667ddac22c38859c9badf5e78194167
Adrian <luiscabreraucme@hotmail.com>
2022-06-25 16:38:39 -0500

    Componente de Formulario

--- presupuesto-app/src/app/formulario/formulario.component.ts
+++ presupuesto-app/src/app/formulario/formulario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+//se pone por defecto el ingreso operacion por el +
+  tipo: String = "ingresoOperacion"; //debe ser igual al valor en el formulario en el parametro value
+  descripcionInput: String;
+  valorInput: number;
+  constructor(private ingresoServicio: IngresoServicio, private egresoServicio: EgresoServicio) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
+
+  tipoOperacion(evento) {
+    this.tipo = evento.target.value;
+  }
+  agregarValor() {
+    if (this.tipo === "ingresoOperacion") {
+        this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput,this.valorInput));
+    }else 
+        this.egresoServicio.egresos.push(new Egreso (this.descripcionInput,this.valorInput));
+    
+  }
+
 
 }
