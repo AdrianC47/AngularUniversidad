9bff936f9561c81c78b6019e0a7befed90fa1530
Adrian <luiscabreraucme@hotmail.com>
2022-06-25 04:06:47 -0500

    Componente de Ingresos

--- presupuesto-app/src/app/ingreso/ingreso.servicio.ts
+++ presupuesto-app/src/app/ingreso/ingreso.servicio.ts
@@ -3,7 +3,15 @@ import { Ingreso } from "./ingreso.model";
 export class IngresoServicio {
     //nombre:tipo
     ingresos:Ingreso[]=[
-        new Ingreso("Salario",4000),
+        new Ingreso("Salario",2500),
         new Ingreso("Venta Coche",5000)
     ];
+
+    eliminar(ingreso:Ingreso){
+        const indice: number =this.ingresos.indexOf(ingreso);
+        //el metodo splice nos sirve para eliminar
+        //los parametros que usa es el indice del elemento que queremos eliminar
+        //y si le pasamos el parametro de 1 estamos indicando cuantos elementos queremos remover
+        this.ingresos.splice(indice,1);
+    }
 }
