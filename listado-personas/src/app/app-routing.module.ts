61f393c81fd52611c592ed2acba2886dc90c8597
Adrian <luiscabreraucme@hotmail.com>
2022-07-13 00:35:01 -0500

    RoutingModule y routing-oulet

--- listado-personas/src/app/app-routing.module.ts
+++ listado-personas/src/app/app-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { PersonasComponent } from './personas/personas.component';
+import { FormularioComponent } from './personas/formulario/formulario.component';
 
 
-
+//declaro la variable
+const routes: Routes=[
+  {path:'',component:PersonasComponent},
+  {path:'personas',component:PersonasComponent},
+  {path:'personas/agregar',component: FormularioComponent},
+  {path:'personas/:id',component:FormularioComponent}, //caso de uso para editar una persona
+]
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  // declarations: [], se quitan las declaraciones y se dejan los imports
+  imports: [RouterModule.forRoot(
+  //se añade el concepto de routing module
+  
+    //pasamos las rutas
+
+    routes
+  )
+  ],
+  //tambien necesitamos exportar el componente , usamos exports
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
