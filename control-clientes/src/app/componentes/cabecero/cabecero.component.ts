45e2ec097b4337e374006045e550a561aa34edc6
Adrian <luiscabreraucme@hotmail.com>
2022-08-15 17:11:19 -0500

    Componente Tablero y Pie de Página

--- control-clientes/src/app/componentes/cabecero/cabecero.component.ts
+++ control-clientes/src/app/componentes/cabecero/cabecero.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cabecero.component.css']
 })
 export class CabeceroComponent implements OnInit {
-  mostrarREsponsive_menu = true;
+  mostrarREsponsive_menu = false;
   constructor() { }
 
   ngOnInit(): void {
-    this.mostrarREsponsive_menu = true;
+
   }
 
 }
