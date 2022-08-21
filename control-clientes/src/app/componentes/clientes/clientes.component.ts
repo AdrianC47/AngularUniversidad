f0f61ddebd6b3288fb4cc29d34e273ad46ac1d28
Adrian <luiscabreraucme@hotmail.com>
2022-08-21 03:36:32 -0500

    Formulario ready modal ready correcion pdf tutorial

--- control-clientes/src/app/componentes/clientes/clientes.component.ts
+++ control-clientes/src/app/componentes/clientes/clientes.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/modelo/cliente.model';
 import { ClienteServicio } from 'src/app/servicios/cliente.service';
-
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-
+  closeModal: string;
   clientes: Cliente[];
-  constructor(private clientesServicio: ClienteServicio) { }
+  constructor(private clientesServicio: ClienteServicio,private modalService: NgbModal) { }
 
   ngOnInit(): void {  
     this.clientesServicio.getClientes().subscribe(
@@ -18,6 +18,22 @@ export class ClientesComponent implements OnInit {
         this.clientes = clientes;
       }
     )
+  }
+  triggerModal(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+      this.closeModal = `Closed with: ${res}`;
+    }, (res) => {
+      this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   getSaldoTotal() {
@@ -31,6 +47,7 @@ export class ClientesComponent implements OnInit {
     }
     return saldoTotal;
   }
+
 
  
 
