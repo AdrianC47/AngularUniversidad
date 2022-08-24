8553a1958d999c21a608a5c08652a44fe5dffc69
Adrian <luiscabreraucme@hotmail.com>
2022-08-24 13:02:36 -0500

    Definición de Variables  con TypeScript

--- TypeScript/HolaMundoTypeScript/funciones.js
+++ TypeScript/HolaMundoTypeScript/funciones.js
@@ -1,5 +1,20 @@
 "use strict";
+//En typescript se usa let para declarar variables a fin de especificar el tipo de dato sin embargo no es necesario
+//porque usa inferencia de tipos, ademas tambien se puede usar var
+let saludo = "Hola amigo desde Typescript";
+//no puede cambiar los tipos de datos una vez inicializados 
+//saludo=10
+saludo = "cambio";
+let numero;
+numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 22;
+const PI = 3.14; //CONSTANTES, ES BUENA PRACTICA A LAS CONSTANTES PONERLAS EN MAYUSCULAS Y SEPARADAS POR _
+//PI=25 NO SE PUEDEN MODIFICAR SU VALOR UNA VEZ INICIALIZADO
 function saludar() {
-    console.log('Hello Friend');
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
 }
 saludar();
--- TypeScript/HolaMundoTypeScript/funciones.ts
+++ TypeScript/HolaMundoTypeScript/funciones.ts
@@ -1,4 +1,20 @@
+//En typescript se usa let para declarar variables a fin de especificar el tipo de dato sin embargo no es necesario
+//porque usa inferencia de tipos, ademas tambien se puede usar var
+let saludo:string="Hola amigo desde Typescript"
+//no puede cambiar los tipos de datos una vez inicializados 
+//saludo=10
+saludo="cambio";
+let numero:number
+numero=10;
+let cualquiera:any;
+cualquiera="cadena";
+cualquiera=22;
+
+const PI=3.14 //CONSTANTES, ES BUENA PRACTICA A LAS CONSTANTES PONERLAS EN MAYUSCULAS Y SEPARADAS POR _
+//PI=25 NO SE PUEDEN MODIFICAR SU VALOR UNA VEZ INICIALIZADO
 function saludar(){
-    console.log('Hello Friend');
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
 }
 saludar()
