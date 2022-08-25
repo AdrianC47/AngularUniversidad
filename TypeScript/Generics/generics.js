911dd283a5726f81c0709bab87b12df30d6365c3
Adrian <luiscabreraucme@hotmail.com>
2022-08-24 23:05:11 -0500

    Definicion de Generics con Typescript

---
+++ TypeScript/Generics/generics.js
@@ -0,0 +1,9 @@
+"use strict";
+//El tema de Generics es basicamente usar un arreglo y especificar el tipo de dato
+//que va a almacenar dicho arreglo. Esto nos permite agregar cierto tipo de información a 
+//nuestros arreglos y por ello es que vamos a indicar el tipo de la siguiente manera
+let arregloNumeros;
+arregloNumeros = [1, 2, 3, 4];
+//esto da error porque no es el mismo tipoarregloNumeros = ["a","b"];
+console.log(arregloNumeros);
+console.log(arregloNumeros[0]);
